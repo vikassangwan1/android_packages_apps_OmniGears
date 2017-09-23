49f2646b08148f2c788b24019afd382de6be6518
beanstown106 <nbenis106@gmail.com>
2017-09-23 16:33:19 +0800

    [2/2] OmniGears: Fingerprint authentication vibration
    Change-Id: Ifbf2cb650083a8bac15d595f4f36cbb2d50187f2

--- src/org/omnirom/omnigears/ButtonSettings.java
+++ src/org/omnirom/omnigears/ButtonSettings.java
@@ -22,6 +22,7 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -60,6 +61,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements OnPref
     private static final String DOUBLE_PRESS_HOME_ACTION = "double_press_home_action";
     private static final String BUTTON_BACK_KILL_TIMEOUT = "button_back_kill_timeout";
     private static final String KEY_BUTTON_LIGHT = "button_brightness";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
 
     private ListPreference mNavbarRecentsStyle;
     private ListPreference mLongPressRecentsAction;
@@ -69,6 +71,8 @@ public class ButtonSettings extends SettingsPreferenceFragment implements OnPref
     private SwitchPreference mDisabkeHWKeys;
     private ListPreference mBackKillTimeout;
     private Preference mButtonLight;
+    private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintVib;
 
     @Override
     public int getMetricsCategory() {
@@ -149,6 +153,16 @@ public class ButtonSettings extends SettingsPreferenceFragment implements OnPref
         mDoublePressHomeAction.setValue(Integer.toString(doublePressHomeAction));
         mDoublePressHomeAction.setSummary(mDoublePressHomeAction.getEntry());
         mDoublePressHomeAction.setOnPreferenceChangeListener(this);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            prefScreen.removePreference(mFingerprintVib);
+        } else {
+            mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+            mFingerprintVib.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -210,6 +224,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements OnPref
             int index = mBackKillTimeout.findIndexOfValue((String) newValue);
             mBackKillTimeout.setSummary(mBackKillTimeout.getEntries()[index]);
             Settings.System.putInt(getContentResolver(), Settings.System.BUTTON_BACK_KILL_TIMEOUT, value);
+            return true;
+        } else if (preference == mFingerprintVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
             return true;
         }
         return false;
