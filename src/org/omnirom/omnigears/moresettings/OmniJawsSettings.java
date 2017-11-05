5fc33d68ce6a48b88139a46d046566f5c7cb3ab1
maxwen <max.weninger@gmail.com>
2017-11-05 15:39:18 +0100

    OmniGears: correct init of default weather icon pack
    Change-Id: I1b42deda948260eba1f94d99124f84fc9f3ca1fa

--- src/org/omnirom/omnigears/moresettings/OmniJawsSettings.java
+++ src/org/omnirom/omnigears/moresettings/OmniJawsSettings.java
@@ -51,6 +51,7 @@ public class OmniJawsSettings extends SettingsPreferenceFragment implements
     private static final String CATEGORY_WEATHER = "weather_category";
     private static final String WEATHER_ICON_PACK = "weather_icon_pack";
     private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
+    private static final String DEFAULT_WEATHER_ICON_PREFIX = "outline";
     private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
     private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
 
@@ -75,7 +76,7 @@ public class OmniJawsSettings extends SettingsPreferenceFragment implements
             String settingHeaderPackage = Settings.System.getString(getContentResolver(),
                     Settings.System.OMNIJAWS_WEATHER_ICON_PACK);
             if (settingHeaderPackage == null) {
-                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE;
+                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE + "." + DEFAULT_WEATHER_ICON_PREFIX;
             }
             mWeatherIconPack = (ListPreference) findPreference(WEATHER_ICON_PACK);
 
@@ -88,7 +89,7 @@ public class OmniJawsSettings extends SettingsPreferenceFragment implements
             int valueIndex = mWeatherIconPack.findIndexOfValue(settingHeaderPackage);
             if (valueIndex == -1) {
                 // no longer found
-                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE;
+                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE + "." + DEFAULT_WEATHER_ICON_PREFIX;
                 Settings.System.putString(getContentResolver(),
                         Settings.System.OMNIJAWS_WEATHER_ICON_PACK, settingHeaderPackage);
                 valueIndex = mWeatherIconPack.findIndexOfValue(settingHeaderPackage);
