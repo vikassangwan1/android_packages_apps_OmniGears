a39c2757537fdd5a6cc6375be01a6336474272ee
Bruno Martins <bgcngm@gmail.com>
2018-03-06 12:53:29 +0000

    OG:  Update the way OMS records details about overlays
    Change-Id: I064380b1f3f57c54fef75c28d410f06488a5ff1e

--- src/org/omnirom/omnigears/interfacesettings/OmniThemePreferenceController.java
+++ src/org/omnirom/omnigears/interfacesettings/OmniThemePreferenceController.java
@@ -142,7 +142,7 @@ public class OmniThemePreferenceController extends AbstractPreferenceController 
     private boolean isChangeableOverlay(String packageName) {
         try {
             PackageInfo pi = mPackageManager.getPackageInfo(packageName, 0);
-            return pi != null && !pi.isStaticOverlay;
+            return pi != null && (pi.overlayFlags & PackageInfo.FLAG_OVERLAY_STATIC) == 0;
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
