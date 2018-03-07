d3bdd776d6643e08c386956232037de703b60c3c
SpiritCroc <spiritcroc@gmail.com>
2018-03-07 16:22:38 +0000

    Return of the ticker scroll [2/2]
    Change-Id: Ifb52c59989609cc63ac750a98292acbf9dd5db37

--- src/org/omnirom/omnigears/interfacesettings/BarsSettings.java
+++ src/org/omnirom/omnigears/interfacesettings/BarsSettings.java
@@ -53,6 +53,7 @@ public class BarsSettings extends SettingsPreferenceFragment implements
     private ListPreference mQuickPulldown;
     private SeekBarPreference mQsPanelAlpha;
     private ListPreference mTickerMode;
+    private ListPreference mTickerAnimation;
 
     @Override
     public int getMetricsCategory() {
@@ -98,6 +99,14 @@ public class BarsSettings extends SettingsPreferenceFragment implements
                 1, UserHandle.USER_CURRENT);
         mTickerMode.setValue(String.valueOf(tickerMode));
         mTickerMode.setSummary(mTickerMode.getEntry());
+
+        mTickerAnimation = (ListPreference) findPreference("status_bar_ticker_animation_mode");
+        mTickerAnimation.setOnPreferenceChangeListener(this);
+        int tickerAnimationMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ANIMATION_MODE,
+                1, UserHandle.USER_CURRENT);
+        mTickerAnimation.setValue(String.valueOf(tickerAnimationMode));
+        mTickerAnimation.setSummary(mTickerAnimation.getEntry());
     }
 
     @Override
@@ -126,7 +135,15 @@ public class BarsSettings extends SettingsPreferenceFragment implements
             int index = mTickerMode.findIndexOfValue((String) newValue);
             mTickerMode.setSummary(
                     mTickerMode.getEntries()[index]);
-            return true;    
+            return true;
+        } else if (preference.equals(mTickerAnimation)) {
+            int tickerAnimationMode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_TICKER_ANIMATION_MODE, tickerAnimationMode, UserHandle.USER_CURRENT);
+            int index = mTickerAnimation.findIndexOfValue((String) newValue);
+            mTickerAnimation.setSummary(
+                    mTickerAnimation.getEntries()[index]);
+            return true;        
         }
         return false;
     }
