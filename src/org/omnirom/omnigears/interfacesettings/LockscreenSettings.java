6b31cd0289a9064bf9fe3093f163f3e0fbe312af
maxwen <max.weninger@gmail.com>
2018-06-07 03:24:31 +0200

    [2/2] OmniGears: lock screen clock style setting
    Change-Id: I7dda1f6e78719daa7206e69a882cfc7819f1964e

--- src/org/omnirom/omnigears/interfacesettings/LockscreenSettings.java
+++ src/org/omnirom/omnigears/interfacesettings/LockscreenSettings.java
@@ -16,12 +16,17 @@
  *
 */
 package org.omnirom.omnigears.interfacesettings;
+
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -33,6 +38,9 @@ import java.util.ArrayList;
 public class LockscreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "LockscreenSettings";
+    private static final String LOCKSCREEN_CLOCK_STYLE = "lockscreen_clock_style";
+
+    private ListPreference mLockscreenClockStyle;
 
     @Override
     public int getMetricsCategory() {
@@ -43,6 +51,15 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen_settings);
+        final ContentResolver resolver = getContentResolver();
+
+        mLockscreenClockStyle = (ListPreference) findPreference(LOCKSCREEN_CLOCK_STYLE);
+        int clockStyle = Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_CLOCK_STYLE, 0);
+
+        mLockscreenClockStyle.setValue(Integer.toString(clockStyle));
+        mLockscreenClockStyle.setSummary(mLockscreenClockStyle.getEntry());
+        mLockscreenClockStyle.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -52,6 +69,13 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenClockStyle) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mLockscreenClockStyle.findIndexOfValue((String) newValue);
+            mLockscreenClockStyle.setSummary(mLockscreenClockStyle.getEntries()[index]);
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_CLOCK_STYLE, value);
+            return true;
+        }
         return false;
     }
 
